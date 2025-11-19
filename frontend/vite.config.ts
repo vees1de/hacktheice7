7d5d3d5fe55abe90b08194b9613d7ea4ac058811
taishin_gn <taishin_gn@albank.ru>
2025-11-19 17:53:18 +0900

    fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     port: 3000
   },
+  publicDir: '/frontend/src/shared/assets/',
   resolve: {
     alias: {
       '@app': resolve(__dirname, './src/app'),
